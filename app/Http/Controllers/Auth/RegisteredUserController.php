5e72400bde270de04e43c04ff1df77385652b6b7
jovianka <jovianpratama123@gmail.com>
2024-12-13 02:55:00 +0800

    Update register controller

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Faculty;
+use App\Models\Major;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -11,6 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Storage;
 
 class RegisteredUserController extends Controller
 {
@@ -31,14 +34,22 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class, 'ends_with:@student.unud.ac.id'],
+            'nim' => ['required', 'string', 'max:255'],
+            'student_proof' => ['required', 'file', 'extensions:pdf'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'phone' => ['required'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'nim' => $request->nim,
+            'student_proof' => $request->file('student_proof')->storeAs('student_proofs', 'student_proof-' . $request->nim . '.pdf'),
+            'phone' => $request->phone,
+            'faculty_id' => $request->faculty_id,
+            'major_id' => $request->major_id,
         ]);
 
         event(new Registered($user));
