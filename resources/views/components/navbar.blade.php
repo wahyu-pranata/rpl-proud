6f327385ef765041a9c387a47a0d15f7a62432ad
Red Coat <bayubujanks@gmail.com>
2024-12-14 20:15:28 +0800

    create navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,33 @@
+<nav class="flex items-center justify-between bg-white px-6 py-4 shadow">
+    <a href="#" class="text-xl font-bold">Proud</a>
+    {{-- tabs --}}
+    <div class="flex items-center space-x-8">
+        @foreach ($tabs as $tab)
+            <a href="" class="flex items-center gap-2 {{ $tab['icon_class'] }}">
+                {{-- icon --}}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
+                >
+                    @foreach ($tab['icon_paths'] as $path)
+                        <path stroke-linecap="round" stroke-linejoin="round" d="{{ $path }}" />
+                    @endforeach
+                </svg>
+                {{-- label --}}
+                <div>{{ $tab['label'] }}</div>
+            </a>
+        @endforeach
+        {{-- profile --}}
+        <a href="" class="flex items-center space-x-2 unselect-tab">
+            <div class="text-end">
+                <div class="font-bold">{{ $dbtop }}</div>
+                <div class="text-sm">{{ $dbbottom }}</div>
+            </div>
+            <div class="w-8 h-8 bg-gray-300 rounded-full"></div> <!-- Placeholder -->
+        </a>
+    </div>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,22 +14,15 @@
   <!-- Styles / Scripts -->
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-    <nav class="navbar">
-        <div class="navbar-brand">
-            <a href="#" class="logo">MyWebsite</a>
-        </div>
-        <ul class="navbar-links">
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
-        <div class="navbar-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-  </nav>
+    @php
+        $tabs = [
+            ['url' => '/home', 'icon_class' => 'home-icon', 'icon_paths' => ['M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z'], 'label' => 'Home'],
+            ['url' => '/profile', 'icon_class' => 'profile-icon', 'icon_paths' => ['M12 12c2.5 0 4.5 2 4.5 4.5S14.5 21 12 21 7.5 19 7.5 16.5 9.5 12 12 12zM12 3c-2.5 0-4.5 2-4.5 4.5S9.5 12 12 12s4.5-2 4.5-4.5S14.5 3 12 3z'], 'label' => 'Profile'],
+            // Add more tabs here
+        ];
+    @endphp
+
+    <x-navbar></x-navbar>
 <body>
 </body>
 
