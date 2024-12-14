b980c2dbc4f14b30745c0b4486cbd02acec8465c
Red Coat <bayubujanks@gmail.com>
2024-12-14 21:46:59 +0800

    fix upload_file

--- resources/js/upload_file.js
+++ resources/js/upload_file.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Select all file inputs, labels, and upload buttons
     const fileInputs = document.querySelectorAll('.file-input');
     const uploadButtons = document.querySelectorAll('.upload-button');
     const fileLabels = document.querySelectorAll('.file-name');
 
     uploadButtons.forEach((button, index) => {
-        const fileInput = fileInputs[index]; // Corresponding file input
-        const fileLabel = fileLabels[index]; // Corresponding label
+        const fileInput = fileInputs[index];
+        const fileLabel = fileLabels[index];
 
         button.addEventListener('click', () => {
             fileInput.click();
@@ -15,14 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
         fileInput.addEventListener('change', (event) => {
             const file = event.target.files[0];
             const fileName = file ? file.name : 'Upload File';
-            const fileType = file ? file.type.split('/').pop() : '';
-            const baseFileName = fileName ? fileName.slice(0, fileName.lastIndexOf('.')) : '';
-            const extension = fileType ? ' (' + fileType + ')' : '';
 
-            // Truncate the file name
-            const truncatedFileName = baseFileName.length > 10 ? `${baseFileName.slice(0, 10)}...${extension}` : `${baseFileName}${extension}`;
+            const maxFileNameLength = 30;
+            const truncatedFileName =
+                fileName.length > maxFileNameLength
+                    ? `${fileName.slice(0, 15)}...${fileName.slice(-10)}`
+                    : fileName;
 
-            // Update the label text
             fileLabel.textContent = truncatedFileName;
         });
     });
