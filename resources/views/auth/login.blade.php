6d6362298b2e0e981ec202bf02183a2b354bcf6d
jovianka <jovianpratama123@gmail.com>
2024-12-17 17:54:54 +0800

    fix login page <head>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,13 +2,12 @@
 <html lang="en">
 
 <head>
-  <<<<<<< HEAD <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('style/login.css') }}">
     @vite('resources/css/login.css')
     <title>Login</title>
-    =======
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -16,7 +15,6 @@
     @vite('resources/css/login.css')
     @vite('resources/js/app.js')
     <title>Login</title>
-    >>>>>>> origin/main
 </head>
 
 <body>
