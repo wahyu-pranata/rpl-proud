8af4017a27e0d8d2291c5b8b70388401e7413663
Wahyu Pranata <wahyupranata131@gmail.com>
2024-12-17 13:37:13 +0800

    Update: dynamic dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,29 +9,43 @@
           <div class="bg-slate-400 rounded-full w-16 h-16">
           </div>
           <div>
-            <h2 class="text-xl font-bold max-w-40 text-nowrap overflow-hidden text-ellipsis hover:overflow-visible">I
-              Komang
-              Wahyu Pranata</h2>
-            <p class="text-slate-400 text-sm">2308561026</p>
+            <h2 class="text-xl font-bold max-w-40 text-nowrap overflow-hidden text-ellipsis hover:overflow-visible">
+              {{ auth()->user()->name }}
+            </h2>
           </div>
-          <table class="border-spacing-4 border-collapse">
-            <tr>
-              <td>Fakultas</td>
-              <td class="pl-6">MIPA</td>
-            </tr>
-            <tr>
-              <td>Program Studi</td>
-              <td class="pl-6">Informatika</td>
-            </tr>
-            <tr>
-              <td>Organisasi</td>
-              <td class="pl-6">-</td>
-            </tr>
-            <tr>
-              <td>No. Telepon</td>
-              <td class="pl-6">081234567890</td>
-            </tr>
-          </table>
+          @if (auth()->user()->type != 'organization')
+            <table class="border-spacing-4 border-collapse">
+              <tr>
+                <td>Fakultas</td>
+                <td class="pl-6">{{ $user->faculty_name }}</td>
+              </tr>
+              <tr>
+                <td>Program Studi</td>
+                <td class="pl-6">{{ $user->major_name }}</td>
+              </tr>
+              <tr>
+                <td>Organisasi</td>
+                <td class="pl-6">{{ $user->type == 'organization' ? $user->name : '-' }}</td>
+              </tr>
+              <tr>
+                <td>No. Telepon</td>
+                <td class="pl-6">{{ $user->phone }}</td>
+              </tr>
+            </table>
+          @else
+            <table class="border-spacing-4 border-collapse">
+              <tr>
+                <td>Lingkupan</td>
+                <td class="pl-6">
+                  {{ ($user->faculty_name == 'Any' ? 'Universitas' : $user->major_name == 'Any') ? 'Fakultas' : 'Program Studi' }}
+                </td>
+              </tr>
+              <tr>
+                <td>Anggota</td>
+                <td class="pl-6">100</td>
+              </tr>
+            </table>
+          @endif
         </div>
       </div>
     </div>
@@ -39,28 +53,61 @@
       Logout
     </button>
   </div>
-  @include('components.calendar')
+  @if (auth()->user()->type == 'organization')
+    <h2 class="font-bold text-2xl">Data Anggota</h2>
+    <x-table :headers="['Nama', 'NIM', 'Program Studi', '']" :rows="[
+        ['Agus 1', '2300000001', 'Informatika'],
+        ['Budi 2', '2300000002', 'Informatika'],
+        ['Caca 3', '2300000003', 'Informatika'],
+        ['Dedi 4', '2300000004', 'Informatika'],
+    ]" />
+  @else
+    @include('components.calendar')
+  @endif
 @endsection
 
 @section('right-section')
-  <h2 class="font-bold text-2xl">Pendaftaran</h2>
-  <x-table :headers="['Kepanitiaan', 'Bidang 1', 'Bidang 2', 'Status', '']" :rows="[
-      ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
-      ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
-      ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
-      ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
-  ]" />
-  <h2 class="font-bold text-2xl">Berjalan</h2>
-  <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Acara Selanjutnya', 'Waktu Acara', '']" :rows="[
-      ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
-      ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
-      ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
-  ]" />
+  @if (auth()->user()->type == 'organization')
+    <h2 class="font-bold text-2xl">Rekrutmen</h2>
+    <x-table :headers="['Kepanitiaan', 'Ketua', 'Jumlah Responden', 'Sisa Waktu', '']" :rows="[
+        ['Invention', 'Agus 1', '20', '3 Hari'],
+        ['Bakti Sosial', 'Agus 1', '20', '3 Hari'],
+        ['Persiapan PKM', 'Agus 1', '20', '3 Hari'],
+        ['Bootcamp', 'Agus 1', '20', '3 Hari'],
+    ]" />
+    <h2 class="font-bold text-2xl">Berjalan</h2>
+    <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Acara Selanjutnya', 'Waktu Acara', '']" :rows="[
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
+    ]" />
 
-  <h2 class="font-bold text-2xl">Riwayat</h2>
-  <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Waktu', '']" :rows="[
-      ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
-      ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
-      ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
-  ]" />
+    <h2 class="font-bold text-2xl">Riwayat</h2>
+    <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Waktu', '']" :rows="[
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
+    ]" />
+  @else
+    <h2 class="font-bold text-2xl">Pendaftaran</h2>
+    <x-table :headers="['Kepanitiaan', 'Bidang 1', 'Bidang 2', 'Status', '']" :rows="[
+        ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
+        ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
+        ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
+        ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
+    ]" />
+    <h2 class="font-bold text-2xl">Berjalan</h2>
+    <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Acara Selanjutnya', 'Waktu Acara', '']" :rows="[
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
+    ]" />
+
+    <h2 class="font-bold text-2xl">Riwayat</h2>
+    <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Waktu', '']" :rows="[
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
+        ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
+    ]" />
+  @endif
 @endsection
