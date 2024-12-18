6a0f458c20131ce09d3dc9f44839c26f2890c42b
Wahyu Pranata <wahyupranata131@gmail.com>
2024-12-19 01:58:50 +0800

    Update: color table

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -59,7 +59,7 @@
   </div>
   @if (auth()->user()->type == 'organization')
     <h2 class="font-bold text-2xl">Data Anggota</h2>
-    <x-table :headers="['Nama', 'NIM', 'Program Studi', '']" :rows="[
+    <x-table color="#679ce4" :headers="['Nama', 'NIM', 'Program Studi', '']" :rows="[
         ['Agus 1', '2300000001', 'Informatika'],
         ['Budi 2', '2300000002', 'Informatika'],
         ['Caca 3', '2300000003', 'Informatika'],
@@ -73,42 +73,42 @@
 @section('right-section')
   @if (auth()->user()->type == 'organization')
     <h2 class="font-bold text-2xl">Rekrutmen</h2>
-    <x-table :headers="['Kepanitiaan', 'Ketua', 'Jumlah Responden', 'Sisa Waktu', '']" :rows="[
+    <x-table color="#2D4970" :headers="['Kepanitiaan', 'Ketua', 'Jumlah Responden', 'Sisa Waktu', '']" :rows="[
         ['Invention', 'Agus 1', '20', '3 Hari'],
         ['Bakti Sosial', 'Agus 1', '20', '3 Hari'],
         ['Persiapan PKM', 'Agus 1', '20', '3 Hari'],
         ['Bootcamp', 'Agus 1', '20', '3 Hari'],
     ]" />
     <h2 class="font-bold text-2xl">Berjalan</h2>
-    <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Acara Selanjutnya', 'Waktu Acara', '']" :rows="[
+    <x-table color="#2D4970" :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Acara Selanjutnya', 'Waktu Acara', '']" :rows="[
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
     ]" />
 
     <h2 class="font-bold text-2xl">Riwayat</h2>
-    <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Waktu', '']" :rows="[
+    <x-table color="#2D4970" :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Waktu', '']" :rows="[
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
     ]" />
   @else
     <h2 class="font-bold text-2xl">Pendaftaran</h2>
-    <x-table :headers="['Kepanitiaan', 'Bidang 1', 'Bidang 2', 'Status', '']" :rows="[
+    <x-table color="#2D4970" :headers="['Kepanitiaan', 'Bidang 1', 'Bidang 2', 'Status', '']" :rows="[
         ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
         ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
         ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
         ['Invention', 'Bidang Lomba', 'Bidang 3D', 'Ditolak'],
     ]" />
     <h2 class="font-bold text-2xl">Berjalan</h2>
-    <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Acara Selanjutnya', 'Waktu Acara', '']" :rows="[
+    <x-table color="#2D4970" :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Acara Selanjutnya', 'Waktu Acara', '']" :rows="[
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', 'Rapat Umum 1', '01/01/2025'],
     ]" />
 
     <h2 class="font-bold text-2xl">Riwayat</h2>
-    <x-table :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Waktu', '']" :rows="[
+    <x-table color="#2D4970" :headers="['Kepanitiaan', 'Bidang', 'Jabatan', 'Waktu', '']" :rows="[
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
         ['Invention', 'Bidang Lomba', 'Sub-Koordinator', '01/01/2025 - 01/09/2025'],
