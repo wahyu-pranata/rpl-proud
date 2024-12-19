36060f5b6d4b42830da5bc3108a187ef84e9cfd2
jovianka <jovianpratama123@gmail.com>
2024-12-19 13:48:00 +0800

    Update navbar and routes

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,30 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Middleware\EnsureUserType;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/data_mhs', function () {
-    return view('data_mhs');
-});
+Route::get('/data_mhs', [RegisteredUserController::class, 'index'])->middleware(['auth', 'verified', EnsureUserType::class.':organization'])->name('data_mhs');
+Route::get('/data_mhs/search', [RegisteredUserController::class, 'search'])->middleware(['auth', 'verified', EnsureUserType::class.':organization'])->name('data_mhs.search');
+Route::patch('/data_mhs/{id}/verify_student_proof', [RegisteredUserController::class, 'verifyStudentProof'])->middleware(['auth', 'verified', EnsureUserType::class.':organization'])->name('data_mhs.verify_student_proof');
+Route::patch('/data_mhs/{id}/unverify_student_proof', [RegisteredUserController::class, 'unverifyStudentProof'])->middleware(['auth', 'verified', EnsureUserType::class.':organization'])->name('data_mhs.unverify_student_proof');
 
 Route::get('/data_panitia', function () {
     return view('data_panitia');
-});
+})->middleware(['auth', 'verified'])->name('data.panitia');;
 
 Route::get('/inbox', function () {
     return view('inbox');
-});
+})->middleware(['auth', 'verified'])->name('inbox');;
 
 Route::get('/inbox_open', function () {
     return view('inbox_open');
-});
+})->middleware(['auth', 'verified'])->name('dashboard');;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
